7d8bf4179d09b3558997ba6ec950c969a2ed8164
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-06-21 00:10:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = "tu_clave_secreta_aqui"
 
 # Configurar base de datos
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicializar base de datos
